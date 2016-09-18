118b5288d78399a662ddca25ce6fd77c9326593d
Daniel Norman <daniel@gudtech.com>
2016-09-18 16:22:28 -0700

    Tidying up before the moving JS toy implementation aside

--- js/test/03-basic-causal.js
+++ js/test/03-basic-causal.js
@@ -48,4 +48,27 @@ describe('basic causal consistency', function() {
 
     it('delivers all messages', () => mesh.deliverAllQueuedMessages() );
 
+
+    // beacons are essentially sparse vector clocks - a compression methond for a vector clock
+    // rather than representing a vector clock as a vector, let is be a set where each clocl is keyed on the id of their respective beacon
+    // beacons would likely have to be originated from a common root beacon in order to guarantee that any party can ultimately materialize the relevant portion of the sparse vector clock when comparing to a formerly uknown beacon.
+    // Each beacon ping would of course contain all causal context for the previous commit cycle,
+    // but subsequent pings would pear back other directly referenced beacons using LRU. When a beacon
+    // version becomes expunged from the active set due to LRU, it is essentially compressed, and remains
+    // determinible by way traversing the former pings until the beacon in question, or the root(or contemporary?)
+    // beacon is arrived at.
+
+    // Q1: is sparse vectorclock compression scheme theoretically equivalent to a non-sparse vector clock? I think so
+    // Q2: is traversal to the root beacon node necessary in very rate circumstances, or frequent circumstances?
+    // Q3: were we to take a root-beacon-less approach, and instead use a number-of-hops limit, would there be any possibility of this being deterministic for other observers? ( presumably not possible )
+    // Q4: confirm: beacons are essentially a way to arrive at commmon points of comparison more often than would occur with fine-grain causal reference alone
+
+    // * for the purposes of initial testing, all nodes can be beacons. back this off using probableistic behavior later
+    // * each beacon ping must reference N other beacons (suggest N=5) of the most active peers (is older causal chain info sufficient to prevent closed cycles among beacons?)
+    // * create 100k slabs
+    // * create a randomized workload on each slab
+    // * measure the occasions of causal fencing
+    // * measure the number of direct causal determinations vs indirect ( beacon traversals )
+
+
 });
