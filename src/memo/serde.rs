ac785fb3d9ccf489976260a22ef9b9afd6629704
Daniel Norman <daniel@gudtech.com>
2017-03-21 02:06:55 -0700

    WIP: stopping for the night. I think I've got things mostly figured out. Famous last words

--- src/memo/serde.rs
+++ src/memo/serde.rs
@@ -98,23 +98,22 @@ enum MemoBodyVariant {
     MemoRequest
 }
 
+const MEMOBODY_VARIANTS: &'static [&'static str] = &[
+    "SlabPresence",
+    "Relation",
+    "Edit",
+    "FullyMaterialized",
+    "PartiallyMaterialized",
+    "Peering",
+    "MemoRequest"
+];
+
 impl<'a> DeserializeSeed for MemoBodySeed<'a> {
     type Value = MemoBody;
     fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
         where D: Deserializer
     {
-
-        const VARIANTS: &'static [&'static str] = &[
-            "SlabPresence",
-            "Relation",
-            "Edit",
-            "FullyMaterialized",
-            "PartiallyMaterialized",
-            "Peering",
-            "MemoRequest"
-        ];
-
-        deserializer.deserialize_enum("MemoBody", VARIANTS, self)
+        deserializer.deserialize_enum("MemoBody", MEMOBODY_VARIANTS, self)
     }
 }
 impl<'a> Visitor for MemoBodySeed<'a> {
@@ -127,9 +126,9 @@ impl<'a> Visitor for MemoBodySeed<'a> {
         where V: EnumVisitor
     {
 
-        match try!(visitor.visit_variant_seed(self)) {
-            (MemoBodyVariant::SlabPresence, variant) => variant.visit_newtype(),
-        //    (MemoBodyVariant::Relation,     variant) => variant.visit_newtype().map(MemoBody::Relation),
+        match try!(visitor.visit_variant()) {
+            (MemoBodyVariant::SlabPresence, variant) => variant.visit_newtype().map(Ok)?, //(MBSlabPresenceVisitor{net: self.net })
+            (MemoBodyVariant::Relation,     variant) => variant.visit_newtype_seed(MBRelationSeed{ net: self.net }).map(Ok)?,
         //    (MemoBodyVariant::Edit, variant) => variant.visit_newtype().map(MemoBody::Edit),
         //    (MemoBodyVariant::FullyMaterialized, variant) => variant.visit_newtype().map(MemoBody::FullyMaterialized),
         //    (MemoBodyVariant::PartiallyMaterialized, variant) => variant.visit_newtype().map(MemoBody::PartiallyMaterialized),
@@ -139,29 +138,131 @@ impl<'a> Visitor for MemoBodySeed<'a> {
     }
 }
 
-impl<'a> DeserializeSeed for MemoBodyVariantSeed<'a> {
+impl Deserialize for MemoBodyVariant {
     fn deserialize<D>(deserializer: D) -> Result<MemoBodyVariant, D::Error>
         where D: Deserializer
     {
-        struct FieldVisitor;
-        deserializer.deserialize(FieldVisitor)
+        deserializer.deserialize(MemoBodyVariantVisitor)
+    }
+}
+struct MemoBodyVariantVisitor;
+impl Visitor for MemoBodyVariantVisitor
+{
+    type Value = MemoBodyVariant;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+       formatter.write_str("RelationSlotId")
+    }
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        match value {
+            "SlabPresence"            => Ok(MemoBodyVariant::SlabPresence),
+            "Relation"                => Ok(MemoBodyVariant::Relation),
+            "Edit"                    => Ok(MemoBodyVariant::Edit),
+            "FullyMaterialized"       => Ok(MemoBodyVariant::FullyMaterialized),
+            "PartiallyMaterialized"   => Ok(MemoBodyVariant::PartiallyMaterialized),
+            "Peering"                 => Ok(MemoBodyVariant::Peering),
+            "MemoRequest"             => Ok(MemoBodyVariant::MemoRequest),
+            _ => Err(serde::de::Error::unknown_field(value, MEMOBODY_VARIANTS)),
+        }
     }
 }
 
-impl Visitor for MemoBodyVariant {
-    type Value = Field;
+struct MBRelationSeed<'a> { net: &'a Network }
+impl<'a> DeserializeSeed for MBRelationSeed<'a> {
+    type Value = MemoBody;
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where D: Deserializer
+    {
+        deserializer.deserialize(self)
+    }
+}
+impl<'a> Visitor for MBRelationSeed<'a> {
+    type Value = MemoBody;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         formatter.write_str("`Ok` or `Err`")
     }
 
-    fn visit_str<E>(self, value: &str) -> Result<Field, E>
-        where E: Error
+    fn visit_map<Visitor>(self, mut visitor: Visitor) -> Result<Self::Value, Visitor::Error>
+        where Visitor: MapVisitor,
     {
-        match value {
-            "Ok" => Ok(Field::Ok),
-            "Err" => Ok(Field::Err),
-            _ => Err(Error::unknown_variant(value, VARIANTS)),
+        let mut values = HashMap::new();
+
+        while let Some((slot, (subject_id,mrh))) = try!(visitor.visit_seed(RelationSlotIdSeed, SubjectMRHSeed{ net: self.net })) {
+            values.insert(slot, (subject_id,mrh));
         }
+
+        Ok(MemoBody::Relation(values))
     }
 }
+
+struct SubjectMRHSeed<'a> { net: &'a Network }
+impl<'a> DeserializeSeed for SubjectMRHSeed<'a> {
+    type Value = (SubjectId,MemoRefHead);
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where D: Deserializer
+    {
+        deserializer.deserialize(self)
+    }
+
+}
+
+impl<'a> Visitor for SubjectMRHSeed<'a> {
+    type Value = (SubjectId,MemoRefHead);
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("Subject+MRH tuple")
+    }
+
+    fn visit_seq<V>(self, mut visitor: V) -> Result<Self::Value, V::Error>
+    where V: SeqVisitor
+    {
+
+        let subject_id : SubjectId = match visitor.visit()? {
+            Some(value) => value,
+            None => {
+                return Err(de::Error::invalid_length(0, &self));
+            }
+        };
+        let mrh : MemoRefHead = match visitor.visit_seed(MemoRefHeadSeed{ net: self.net })? {
+            Some(value) => value,
+            None => {
+                return Err(de::Error::invalid_length(1, &self));
+            }
+        };
+
+        Ok((subject_id,mrh))
+    }
+}
+
+/*
+struct MemoBodyBasicVisitor;
+
+impl de::Visitor for MemoBodyBasicVisitor {
+    type Value = MemoBody;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("MemoBody")
+    }
+
+    fn visit_seq<V>(self, visitor: V) -> Result<Self::Value, V::Error>
+        where V: SeqVisitor
+    {
+        (MemoBodyVariant::SlabPresence, variant) => variant.visit_newtype().map(Ok)?, //(MBSlabPresenceVisitor{net: self.net })
+        (MemoBodyVariant::Edit, variant) => variant.visit_newtype().map(MemoBody::Edit),
+        _ => Err(serde::de::Error::unknown_field(value, MEMOBODY_VARIANTS)),
+
+    }
+
+    // Similar for other methods:
+    //   - visit_i16
+    //   - visit_u8
+    //   - visit_u16
+    //   - visit_u32
+    //   - visit_u64
+}
+*/
