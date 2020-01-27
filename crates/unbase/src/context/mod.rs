81f73f32e6309f08919e945bb43a3a65d14d8685
Daniel Norman <daniel@danielnorman.net>
2020-01-27 13:12:14 -0800

    consolidate context/internal and interface into mod.rs, refactor to remove recursion for async functions insert and scan

--- crates/unbase/src/context/mod.rs
+++ crates/unbase/src/context/mod.rs
@@ -1,21 +1,38 @@
-mod internal;
 pub mod stash;
-mod interface;
 
 use crate::{
     error::{
+        WriteError,
         RetrieveError,
     },
     index::IndexFixed,
-    slab::SlabHandle,
-    subjecthandle::SubjectHandle,
+    slab::{
+        SlabHandle,
+        EdgeLink,
+        EdgeSet,
+        MemoBody,
+        MemoId,
+        RelationSet,
+        RelationSlotId,
+    },
+    subject::{
+        SubjectType,
+        SubjectId,
+        Subject,
+    },
+    subjecthandle::{
+        SubjectHandle
+    },
+    memorefhead::MemoRefHead,
 };
 
 use self::stash::Stash;
 use timer::Delay;
 
 use std::{
-    sync::{Arc, RwLock},
+    collections::HashMap,
+    fmt,
+    sync::{Arc, Mutex},
     ops::Deref,
     time::{Instant, Duration},
 };
@@ -37,7 +54,7 @@ pub struct Context(Arc<ContextInner>);
 
 pub struct ContextInner {
     pub slab: SlabHandle,
-    pub root_index: RwLock<Option<Arc<IndexFixed>>>,
+    pub root_index_node: Arc<Mutex<Option<Subject>>>,
     applier: RemoteHandle<()>,
     stash: Stash,
     //pathology:  Option<Box<Fn(String)>> // Something is wrong here, causing compile to fail with a recursion error
@@ -83,14 +100,14 @@ impl Context {
 
         let inner = ContextInner {
             slab: slab,
-            root_index: RwLock::new(None),
+            root_index: Arc::new(Mutex::new(None)),
             stash,
             applier,
         };
 
         Context(Arc::new(inner))
     }
-    pub async fn try_fetch_kv(&mut self, key: &str, val: &str) -> Result<Option<SubjectHandle>, RetrieveError> {
+    pub async fn try_fetch_kv(&self, key: &str, val: &str) -> Result<Option<SubjectHandle>, RetrieveError> {
         // TODO implement field-specific indexes
         //if I have an index for that field {
         //    use it
@@ -116,6 +133,241 @@ impl Context {
 
             Delay::new(Duration::from_millis(50)).await;
         }
+    }
+    /// Retrive a Subject from the root index by ID
+    pub async fn get_subject_by_id(&self, subject_id: SubjectId) -> Result<Option<SubjectHandle>, RetrieveError> {
+
+        let root_index = self.root_index(Duration::from_secs(5)).await?;
+
+        match root_index.get(&self, subject_id.id).await? {
+            Some(s) => {
+                let sh = SubjectHandle{
+                    id: subject_id,
+                    subject: s,
+                    context: self.clone()
+                };
+
+                Ok(Some(sh))
+            },
+            None => Ok(None)
+        }
+    }
+
+    pub fn concise_contents (&self) -> Vec<String> {
+        self.stash.concise_contents()
+    }
+
+    // Magically transport subject heads into another context in the same process.
+    // This is a temporary hack for testing purposes until such time as proper context exchange is enabled
+    // QUESTION: should context exchanges be happening constantly, but often ignored? or requested? Probably the former,
+    //           sent based on an interval and/or compaction ( which would also likely be based on an interval and/or present context size)
+    #[tracing::instrument]
+    pub async fn hack_send_context(&self, other: &Context) -> Result<usize,WriteError> {
+        self.compact().await?;
+
+        let from_slabref = other.slab.agent.localize_slabref(&self.slab.my_ref);
+
+        let mut memoref_count = 0;
+
+        for head in self.stash.iter() {
+            memoref_count += head.len();
+
+            let apply_head = other.slab.agent.localize_memorefhead(&head, &from_slabref, false);
+            other.apply_head(&apply_head).await?;
+        }
+
+        Ok(memoref_count)
+    }
+    pub async fn get_relevant_subject_head(&self, subject_id: SubjectId) -> Result<MemoRefHead, RetrieveError> {
+        match subject_id {
+            SubjectId{ stype: SubjectType::IndexNode,.. } => {
+                Ok(self.stash.get_head(subject_id).clone())
+            },
+            SubjectId{ stype: SubjectType::Record, .. } => {
+                // TODO: figure out a way to noop here in the case that the SubjectHead in question
+                //       was pulled against a sufficiently identical context stash state.
+                //       Perhaps stash edit increment? how can we get this to be really granular?
+
+                let root_index = self.root_index(Duration::from_secs(5)).await?;
+
+                match root_index.get_head(&self, subject_id.id).await? {
+                    Some(mrh) => Ok(mrh),
+                    None      => Ok(MemoRefHead::Null)
+                }
+            }
+        }
+    }
+    pub fn try_root_index_node(&self) -> Result<(),RetrieveError> {
+        {
+            let rg = self.root_index.lock().unwrap();
+            if let Some( ref arcindex ) = *rg {
+
+            }
+        };
+
+        let seed = self.slab.net.get_root_index_seed(&self.slab);
+        if seed.is_some() {
+            let index = IndexFixed::new_from_memorefhead(&self, 5, seed);
+            let arcindex = Mutex::new(Some(index));
+            *self.root_index.lock().unwrap() = Some(arcindex.clone());
+
+            Ok(())
+        }else{
+            Err(RetrieveError::IndexNotInitialized)
+        }
+
+    }
+    pub async fn root_index_node (&self, wait: Duration) -> Result<Subject, RetrieveError> {
+        let start = Instant::now();
+
+        loop {
+            if start.elapsed() > wait{
+                return Err(RetrieveError::NotFoundByDeadline)
+            }
+
+            if let Ok(ri) = self.try_root_index_node() {
+                return Ok(ri);
+            };
+
+            Delay::new(Duration::from_millis(50)).await;
+        }
+    }
+    pub fn get_resident_subject_head(&self, subject_id: SubjectId) -> MemoRefHead {
+        self.stash.get_head(subject_id).clone()
+    }
+    pub fn get_resident_subject_head_memo_ids(&self, subject_id: SubjectId) -> Vec<MemoId> {
+        self.get_resident_subject_head(subject_id).memo_ids()
+    }
+    pub fn cmp(&self, other: &Self) -> bool {
+        // stable way:
+        &*(self.0) as *const _ != &*(other.0) as *const _
+
+        // unstable way:
+        // Arc::ptr_eq(&self.inner,&other.inner)
+    }
+
+    pub async fn add_test_subject(&self, subject_id: SubjectId, relations: Vec<MemoRefHead>) -> MemoRefHead {
+
+        let mut edgeset = EdgeSet::empty();
+
+        for (slot_id, mrh) in relations.iter().enumerate() {
+            if let &MemoRefHead::Subject{..} = mrh {
+                edgeset.insert(slot_id as RelationSlotId, mrh.clone())
+            }
+        }
+
+        let head = self.slab.new_memo(
+            Some(subject_id),
+            MemoRefHead::Null,
+            MemoBody::FullyMaterialized { v: HashMap::new(), r: RelationSet::empty(), e: edgeset, t: subject_id.stype }
+        ).to_head();
+
+        self.apply_head(&head).await.expect("apply head")
+    }
+
+    /// Attempt to compress the present query context.
+    /// We do this by issuing Relation memos for any subject heads which reference other subject heads presently in the query context.
+    /// Then we can remove the now-referenced subject heads, and repeat the process in a topological fashion, confident that these
+    /// referenced subject heads will necessarily be included in subsequent projection as a result.
+    pub async fn compact(&self) -> Result<(), WriteError>  {
+        let before = self.stash.concise_contents();
+
+        //TODO: implement topological MRH iterator for stash
+        //      non-topological iteration will yield sub-optimal compaction
+
+        // iterate all heads in the stash
+        for parent_mrh in self.stash.iter() {
+            let mut updated_edges = EdgeSet::empty();
+
+            for edgelink in parent_mrh.project_occupied_edges(&self.slab).await? {
+                if let EdgeLink::Occupied{slot_id,head:edge_mrh} = edgelink {
+
+                    if let Some(subject_id) = edge_mrh.subject_id(){
+                        if let stash_mrh @ MemoRefHead::Subject{..} = self.stash.get_head(subject_id) {
+                            // looking for cases where the stash is fresher than the edge
+                            if stash_mrh.descends_or_contains(&edge_mrh, &self.slab).await?{
+                                updated_edges.insert( slot_id, stash_mrh );
+                            }
+                        }
+                    }
+                }
+            }
+
+            if updated_edges.len() > 0 {
+                // TODO: When should this be materialized?
+                let memobody = MemoBody::Edge(updated_edges);
+                let subject_id = parent_mrh.subject_id().unwrap();
+
+                let head = self.slab.new_memo(
+                    Some(subject_id),
+                    MemoRefHead::Null,
+                    memobody.clone()
+                ).to_head();
+
+                self.apply_head(&head).await?;
+            }
+        }
+
+        println!("COMPACT Before: {:?}, After: {:?}", before, self.stash.concise_contents() );
+        Ok(())
+    }
+
+    pub fn is_fully_materialized(&self) -> bool {
+        unimplemented!();
+        // for subject_head in self.manager.subject_head_iter() {
+        //     if !subject_head.head.is_fully_materialized(&self.slab) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+    }
+    pub (crate) async fn insert_into_root_index(&self, subject_id: SubjectId, subject: &Subject) -> Result<(),WriteError> {
+        self.root_index(Duration::from_secs(5)).await?.insert(self, subject_id.id, subject).await
+    }
+
+    /// Called by the Slab whenever memos matching one of our subscriptions comes in, or by the Subject when an edit is made
+    pub (crate) async fn apply_head(&self, head: &MemoRefHead) -> Result<MemoRefHead,WriteError> {
+        // println!("Context.apply_subject_head({}, {:?}) ", subject_id, head.memo_ids() );
+        self.stash.apply_head(&self.slab, head).await
+    }
+    pub (crate) async fn get_subject(&self, subject_id: SubjectId) -> Result<Option<Subject>, RetrieveError> {
+        // TODO centralized timeout duration config
+        let root_index = self.root_index(Duration::from_secs(5)).await?;
+        root_index.get(self, subject_id.id).await
+    }
+    /// Retrieve a subject for a known MemoRefHead – ususally used for relationship traversal.
+    /// Any relevant context will also be applied when reconstituting the relevant subject to ensure that our consistency model invariants are met
+    pub async fn get_subject_with_head(&self,  mut head: MemoRefHead)  -> Result<Subject, RetrieveError> {
+
+        if head.len() == 0 {
+            return Err(RetrieveError::InvalidMemoRefHead);
+        }
+
+        if let Some(subject_id) = head.subject_id() {
+            head.apply_mut(&self.stash.get_head(subject_id), &self.slab ).await?;
+        }
+
+        let subject = Subject::reconstitute(&self, head)?;
+        return Ok(subject);
+
+    }
+    pub (crate) async fn get_subject_handle_with_head (&self, head: MemoRefHead)  -> Result<SubjectHandle, RetrieveError> {
+        Ok(SubjectHandle{
+            id: head.subject_id().ok_or( RetrieveError::InvalidMemoRefHead )?,
+            subject: self.get_subject_with_head(head).await?,
+            context: self.clone()
+        })
+    }
+}
+
+
+impl fmt::Debug for Context {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+
+        fmt.debug_struct("ContextShared")
+            .field("subject_heads", &self.stash.subject_ids() )
+            .finish()
     }
 }
 
